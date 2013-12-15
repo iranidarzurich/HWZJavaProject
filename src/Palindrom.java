7c52ba437332b886fc954bbe12a1c2e041d8e944
chanty7 <chantal.schoch@hotmail.com>
2013-12-15 11:59:41 +0100

    Stimmt das?

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,17 +1,18 @@
 /**
  * 
  * @author Chantal Schoch
- * @task palindrom Instanzmedthodenanalyse
+ * @task palindrom Klassenmedthodenanalyse
  * 
  */
-public class Palindrom2 {
-	static String zeichenkette = "adida";
+public class Palindrom {
+	String zeichenkette = "adida";
 
 	public static void main(String[] args) {
-		StringBuffer str = new StringBuffer(zeichenkette);
+		Palindrom p = new Palindrom();
+		StringBuffer str = new StringBuffer(p.zeichenkette);
 		StringBuffer s = str.reverse();
 		String s1 = s.toString();
-		String s2 = zeichenkette.toString();
+		String s2 = p.zeichenkette.toString();
 
 		if (s2.equals(s1)) {
 			System.out.println("Palindrom");
