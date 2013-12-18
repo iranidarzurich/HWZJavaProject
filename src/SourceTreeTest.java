36b38d54e5d1e798fc46434f8a7600e5edcd6928
Mehdi <mehdi.geiger@gmail.com>
2013-12-18 20:42:04 +0100

    Einsatz von Source Tree

--- src/SourceTreeTest.java
+++ src/SourceTreeTest.java
@@ -5,6 +5,10 @@ public class SourceTreeTest {
 		int a=2;
 		double b=3;
 		String name= "Chantal";
+		/**
+		 * Falls dass das gsehnd :D Ich möcht euch mitteile dass ich im project source tree einsetze möcht
+		 * Gruss Mehdi
+		 */
 
 	}
 
